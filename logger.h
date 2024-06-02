e40fb87f88f9e73e113d5652b7d2b906e75b32e9
ABN <75630937+PrinceABN@users.noreply.github.com>
2024-06-02 17:17:04 +0300

    Update logger.h

--- logger.h
+++ logger.h
@@ -1,14 +1,47 @@
-#ifndef LOGGER_H
-#define LOGGER_H
+#pragma once
 
-#include <string>
+namespace logger {
 
-class Logger {
-public:
-    static void error(const std::string& message);
-    static void warning(const std::string& message);
-    static void success(const std::string& message);
-    static void log(const std::string& message);
-};
+	enum e_logtype : int {
+		success = 0,
+		info = 1,
+		error = 2
+	};
 
-#endif // LOGGER_H
+
+	void log(e_logtype type, std::string msg, ...) {
+
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		const char* prefix = "";
+
+		switch (type) {
+		case e_logtype::success:
+			SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+			prefix = "[#]";
+			break;
+		case e_logtype::info:
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+			prefix = "[~]";
+			break;
+		case e_logtype::error:
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+			prefix = "[!]";
+			break;
+		}
+		std::string final_string = prefix + msg;
+		va_list args;
+		va_start(args, msg);
+		std::vprintf(final_string.c_str(), args);
+		va_end(args);
+		SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+	}
+
+}
+
+/*
+SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+*/
